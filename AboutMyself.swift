ca27a816201738005848791e8f046d7ba07be429
RaghavIsCoding <142236771+RaghavIsCoding@users.noreply.github.com>
2023-09-15 00:42:42 +0530

    Create AboutMyself.swift

---
+++ AboutMyself.swift
@@ -0,0 +1,41 @@
+let name:String = "Raghav"
+let sisterName = "Megha"
+let fatherName = "Mr.RamChandra"
+let motherName = "Mrs.Rukmani"
+
+var surname = "Maurya"
+
+print("My name is ", name, surname)
+print("My mother name is ", motherName, surname)
+print("My father name is ", fatherName, surname)
+print("My sister name is ", sisterName, surname)
+
+surname = "Kashyap"
+var Driver:String = "Rajeev"
+print("\nDriver: ", Driver, surname, "\n")
+
+var age:Int = 11
+let fatherPhone = 9411229528
+let motherPhone = 9548186538
+
+print("Age of ", name, " is", age, "\n")
+print("Contact\n")
+print("Father Phone Number: ", fatherPhone)
+print("Mother Phone Number: ", motherPhone)
+
+var FirstHobby = "Math"
+var SecondHobby = "Coding"
+
+print("\nHobby\n")
+print("Hobbies")
+print(FirstHobby)
+print(SecondHobby)
+
+print("\t\t\tParagraph")
+print("I hear about coding in HTML CSS and JavaScript by Manoj Saru")
+print("I love Maths and Coding")
+
+var bye = "BYE BYE"
+print(bye)
+
+print("\t\t-", name)
